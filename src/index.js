2b9f2f8396205255cad9a8753485a55446b8f31a
tataurova <tataurovaoe@yandex.ru>
2020-10-08 02:22:51 +0300

    feat: implement check function for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pairs = [];
+    bracketsConfig.forEach((item) => {
+        pairs.push(item.join(``));
+    })
+
+    while (pairs.some(pair => str.includes(pair))) {
+        bracketsConfig.forEach((arr) => {
+            let pairForDelete = arr.join(``);
+            while (str.includes(pairForDelete)) {
+                str = str.replace(pairForDelete, ``);
+            }
+        });
+    }
+
+    return str.length === 0;
 }
